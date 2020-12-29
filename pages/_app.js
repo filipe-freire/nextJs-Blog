c5585580a0a512e07e7895c6babd601c0483debc
Filipe <filipefreire@protonmail.com>
2020-12-29 00:02:00 +0000

    -Add HighlightJS component & nextConfig

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import "highlight.js/styles/ir-black.css" // highlight.js/static/demo
 import "styles/index.scss"
 
 export default ({Component, pageProps}) => <Component {...pageProps} />

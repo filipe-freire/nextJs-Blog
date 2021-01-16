c98067cb79e07391cc9f707d8bcfcd3dfc0260d7
Filipe <filipefreire@protonmail.com>
2021-01-16 20:27:42 +0000

    -Add Footer Icons

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,17 +1,58 @@
-import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Footer = () => {
   return (
     <footer className="page-footer">
       <div>
-        <a href="#">courses</a>
-        {" | "}
-        <a href="#">github</a>
-        {" | "}
-        <a href="#">facebook</a>
+        <a href="https://twitter.com/Filipe__Freire" rel="noopener noreferrer" target="_blank">
+          <FontAwesomeIcon icon={['fab', 'twitter']} /> Twitter
+        </a>
+        {' | '}
+        <a href="https://github.com/filipe-freire/" rel="noopener noreferrer" target="_blank">
+          <FontAwesomeIcon icon={['fab', 'github']} /> Github
+        </a>
+        {' | '}
+        <a href="https://linkedin.com/in/filiperpfreire/" rel="noopener noreferrer" target="_blank">
+          <FontAwesomeIcon icon={['fab', 'linkedin']} /> LinkedIn
+        </a>
       </div>
     </footer>
   );
 };
 
+// <footer class="footer">
+//       <p>Want to reach out? Find me on:</p>
+//       <ul class="social-list">
+//         <li class="social-list__item">
+//           <a
+//             class="social-list__link"
+//             href="https://twitter.com/Filipe__Freire"
+//             target="_blank"
+//             rel="noreferrer"
+//           >
+//             <i class="fab fa-twitter fa-2x"></i>
+//           </a>
+//         </li>
+//         <li class="social-list__item">
+//           <a
+//             class="social-list__link"
+//             href="https://github.com/filipe-freire/"
+//             target="_blank"
+//             rel="noreferrer"
+//           >
+//             <i class="fab fa-github fa-2x"></i>
+//           </a>
+//         </li>
+//         <li class="social-list__item">
+//           <a
+//             class="social-list__link"
+//             href="https://linkedin.com/in/filiperpfreire/"
+//             target="_blank"
+//             rel="noreferrer"
+//           >
+//             <i class="fab fa-linkedin fa-2x"></i>
+//           </a>
+//         </li>
+//       </ul>
+//     </footer>
 export default Footer;
--- pages/_app.js
+++ pages/_app.js
@@ -8,9 +8,20 @@ import {
   faSortNumericDown,
   faSortNumericUp
 } from '@fortawesome/free-solid-svg-icons';
+import { faTwitter, faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
 
 config.autoAddCss = false;
-library.add(faList, faBorderAll, faSortNumericDown, faSortNumericUp, faSun, faMoon);
+library.add(
+  faList,
+  faBorderAll,
+  faSortNumericDown,
+  faSortNumericUp,
+  faSun,
+  faMoon,
+  faTwitter,
+  faLinkedin,
+  faGithub
+);
 
 import '@fortawesome/fontawesome-svg-core/styles.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -18,15 +29,15 @@ import 'highlight.js/styles/ir-black.css'; // highlight.js/static/demo
 import 'react-toggle/style.css'; // Theme Toggler Button
 import 'styles/index.scss';
 
-export default ({ Component, pageProps }) => (
-  <ThemeProvider>
-    <Component {...pageProps} />
-  </ThemeProvider>
-);
-
-// const App = ({ Component, pageProps }) => (
+// export default ({ Component, pageProps }) => (
 //   <ThemeProvider>
-//     <Component {...pageProps} />;
+//     <Component {...pageProps} />
 //   </ThemeProvider>
 // );
-// export default App;
+
+const App = ({ Component, pageProps }) => (
+  <ThemeProvider>
+    <Component {...pageProps} />;
+  </ThemeProvider>
+);
+export default App;
